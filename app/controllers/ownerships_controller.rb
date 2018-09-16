e2f1351a32ec49d8e83f0168ad1e424c96a8dffa
yuki.kaneko <ykaneko204@gmail.com>
2018-09-16 15:12:17 +0000

    Have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -16,6 +16,12 @@ class OwnershipsController < ApplicationController
       flash[:success] = '商品を Want しました。'
     end
 
+    # Have 関係として保存    
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
+    end
+
     redirect_back(fallback_location: root_path)
   end
 
@@ -26,7 +32,12 @@ class OwnershipsController < ApplicationController
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
     end
-
+    
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
+    end
+    
     redirect_back(fallback_location: root_path)
   end
 end
